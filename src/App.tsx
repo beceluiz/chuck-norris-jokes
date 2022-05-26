fc4a45f1e85234ba7a6e414e579e8e097024309b
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-05-26 15:53:57 -0300

    Style: responsible fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   
   return <div className='block items-center'>
     <div className='px-2.5 py-2.5 mx-auto'>
-      <TestimonialItem  texto={Dados}/>
+      <TestimonialItem texto={Dados}/>
       </div>
       <span className='flex justify-center px-1.5 py-1.5'>
         <button className='text-sky-100 hover:text-black border border-sky-300 hover:bg-sky-100 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ' onClick={()=> {
--- src/components/TestimonialItem.tsx
+++ src/components/TestimonialItem.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function TestimonialItem({texto}:any) {
   return (
-    <div>
+    <div className='mt-10 mb-2'>
      
-      <img className='w-60 h-60 rounded-full  mx-auto bg-sky-100 ' src="/chuck-img.png"/> 
-    <div className='mt-2 bg-sky-100 pt-2 p-8 rounded-xl max-w-xl mx-auto'>
+      <img className='mb-4 w-60 h-60 rounded-full  mx-auto bg-sky-100 ' src="/chuck-img.png"/> 
+    <div className='mt-2 bg-sky-100 pt-4 p-8 rounded-xl max-w-xl mx-auto'>
       
         <div className='pt-6 text-center space-y-4 text-lg font-semibold font-sans'>" {texto}"
         </div>
