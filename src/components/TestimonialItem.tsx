e4e223d0a60f3814d86c68f00addd4ebea25ed31
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-05-26 15:43:28 -0300

    Style: Fixing placeholder padding

--- src/components/TestimonialItem.tsx
+++ src/components/TestimonialItem.tsx
@@ -5,7 +5,7 @@ function TestimonialItem({texto}:any) {
     <div>
      
       <img className='w-60 h-60 rounded-full  mx-auto bg-sky-100 ' src="/chuck-img.png"/> 
-    <div className='mt-2 bg-sky-100 p-8 rounded-xl max-w-xl mx-auto'>
+    <div className='mt-2 bg-sky-100 pt-2 p-8 rounded-xl max-w-xl mx-auto'>
       
         <div className='pt-6 text-center space-y-4 text-lg font-semibold font-sans'>" {texto}"
         </div>
