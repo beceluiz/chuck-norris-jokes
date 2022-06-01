fd1fe88de7a895be3c2a75efee274a2c48f6bf01
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-01 14:57:27 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 import TestimonialItem from './components/TestimonialItem';
 
 function App() {
-  const [Joke, setJoke] = useState('');
-  const [ShownJoke, setShownJoke] = useState('');
+  const [Joke, setJoke] = useState(null);
+  const [ShownJoke, setShownJoke] = useState(null);
   useEffect(() => setShownJoke(Joke), [Joke])
 
 
@@ -17,7 +17,7 @@ function App() {
   
   return <div className='block items-center'>
     <div className='px-2.5 py-2.5 mx-auto'>
-      <TestimonialItem texto={ShownJoke === '' ? 'Chuck Norris would take 1 second to code this app' : ShownJoke}/>
+      <TestimonialItem texto={ShownJoke || 'Chuck Norris would take 1 second to code this app'}/>
       </div>
       <span className='flex justify-center px-1.5 py-1.5'>
         <button className='text-sky-100 hover:text-black border border-sky-300 hover:bg-sky-100 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ' onClick={updateJoke}>Next Joke</button>
